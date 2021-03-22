dc2633744500b904989c629e9a0ea7ec5c1c2cfd
Sverre Holm <la3za@users.noreply.github.com>
2021-03-22 15:51:00 +0100

    Revert "LA3ZA in startup +"
    This reverts commit 955361619e66b42faba5cd83952761e8051e184f.

--- k3ng_keyer/la3za.h
+++
@@ -1,8 +0,0 @@
-
-// Variable macros
-
-// make it 100 or so to repeat indefinetely as before:
-#define PRACTICE_MAX_ATTEMPTS 4 
-
-// truncate callsign in receive-send practice mode
-#define PRACTICE_MAX_CALLSIGN_LENGTH 5
