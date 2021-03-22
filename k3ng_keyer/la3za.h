fb8f9feba5be6817a349462081ee736651d238f5
Sverre Holm <la3za@users.noreply.github.com>
2021-03-22 15:49:48 +0100

    Revert "Customized startup"
    This reverts commit c420698f4b2227913d6d0da12982ac66d07d66e0.

--- k3ng_keyer/la3za.h
+++ k3ng_keyer/la3za.h
@@ -2,8 +2,7 @@
 // Variable macros
 
 // make it 100 or so to repeat indefinetely as before:
-#define PRACTICE_MAX_ATTEMPTS 4 // doesn't work anymore in 2021
+#define PRACTICE_MAX_ATTEMPTS 4 
 
 // truncate callsign in receive-send practice mode
-// no place in 2021 version
-//#define PRACTICE_MAX_CALLSIGN_LENGTH 5
+#define PRACTICE_MAX_CALLSIGN_LENGTH 5
